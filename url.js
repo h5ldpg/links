4299c9a84a50f42eff926e8fcfae4d3e1ff36c60
h5ldpg <110018528+h5ldpg@users.noreply.github.com>
2024-10-15 13:04:37 +0800

    Update url.js

--- url.js
+++ url.js
@@ -1,6 +1,6 @@
-<script>    // 设置WhatsApp链接
-    document.getElementById('whatsapp-button').href = 'https://wa.me/1234567890'; // 替换为实际WhatsApp号码
 
-    // 设置LINE链接
-    document.getElementById('line-button').href = 'https://line.me/ti/p/yourlineid'; // 替换为实际LINE ID
-</script>
+// 设置 WhatsApp 链接
+document.getElementById('whatsapp-button').href = 'https://wa.me/1234567890'; // 替换为实际 WhatsApp 号码
+
+// 设置 LINE 链接
+document.getElementById('line-button').href = 'https://line.me/ti/p/yourlineid'; // 替换为实际 LINE ID
